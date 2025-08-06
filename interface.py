87c512d41404df61e6977091ef6ea404e06be9eb
CheckmateLL <saoirse.girl47@gmail.com>
2025-08-06 13:13:43 +0300

    plan for GUI

--- interface.py
+++ interface.py
@@ -1,1 +1,10 @@
 #something someday
+#here should be interface for GUI
+
+
+#file load
+#path to save files (default to same folder as original)
+#checkboxes for checks
+#custom parameters for each check (default as in files)
+#report
+#log
