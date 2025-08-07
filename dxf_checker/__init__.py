35c3527a8625d3fd2a5654c8afc2d4924ddfe9dd
CheckmateLL <saoirse.girl47@gmail.com>
2025-08-07 17:14:49 +0300

    bump: version 1.1.5 → 1.2.0

--- dxf_checker/__init__.py
+++ dxf_checker/__init__.py
@@ -2,6 +2,6 @@
 CheckMateLL DXF Checker - A tool for validating DXF segment integrity
 """
 
-__version__ = "1.1.5" # {bumpver}
+__version__ = "1.2.0" # {bumpver}
 __author__ = "Juliya Lehka"
 __email__ = "julilya.legkaya@gmail.com"
